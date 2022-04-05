ba9aeee6e670a6a0e8f45b62441aa7adc894cfe4
elsayed hussein <elsayed.hussen@gmail.com>
2022-04-05 17:28:06 +0200

    one step to finsh

--- main.js
+++ main.js
@@ -1,32 +1,18 @@
 const app = Vue.createApp({
   data() {
     return {
-      cart: 0,
-      product: "Socks",
-      image: "./assets/images/socks_blue.jpg",
-      inStock: true,
-      inventory: 100,
-      details: ["50% cotton", "30% wool", "20% polyester"],
-      variants: [
-        {
-          id: 2234,
-          color: "green",
-          image: "./assets/images/socks_green.jpg",
-        },
-        {
-          id: 2235,
-          color: "blue",
-          image: "./assets/images/socks_blue.jpg",
-        },
-      ],
+      cart: [],
+      // cart: 0,
+      // premium: true,
+      premium: false,
     };
   },
   methods: {
-    addToCart() {
-      this.cart += 1;
+    updateCart(id) {
+      this.cart.push(id);
     },
-    updateImage(variantImage) {
-      this.image = variantImage;
-    },
+    // updateCart() {
+    //   this.cart += 1;
+    // },
   },
 });
